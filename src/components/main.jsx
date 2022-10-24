99794e146ac42268c15ed8be9712a49868904a0c
Adeboluwarin <adeboluwarinthomas@gmail.com>
2022-10-24 17:00:14 +0100

    Added the hover feature for the link section

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -9,37 +9,37 @@ const main = () => {
        <p className='hidden'id='slack'>Adeboluwarin Thomas</p>
        <div>
         <a href='https://twitter.com/adeboluuu'>
-            <button id='twitter__link' className='bg-[#EAECF0] w-11/12 h-14 rounded-lg border-[#EAECFO] my-2 text-lg font-medium'>Twitter Link</button>
+            <button id='twitter__link' className='bg-[#EAECF0] hover:bg-[#D0D5DD] w-11/12 h-14 rounded-lg border-[#EAECFO] my-2 text-lg font-medium'>Twitter Link</button>
         </a>
        </div>
 
        <div>
         <a href='https://training.zuri.team/'>
-            <button id='btn__zuri'className='bg-[#EAECF0] w-11/12 h-14 rounded-lg border-[#EAECFO] my-2 text-lg font-medium'>Zuri Team</button>
+            <button id='btn__zuri'className='bg-[#EAECF0] hover:bg-[#D0D5DD] w-11/12 h-14 rounded-lg border-[#EAECFO] my-2 text-lg font-medium'>Zuri Team</button>
         </a>
        </div>
 
        <div>
         <a href='http://books.zuri.team/'>
-            <button id='books'className='bg-[#EAECF0] w-11/12 h-14 rounded-lg border-[#EAECFO] my-2 text-lg font-medium'>Zuri Books</button>
+            <button id='books'className='bg-[#EAECF0] hover:bg-[#D0D5DD] w-11/12 h-14 rounded-lg border-[#EAECFO] my-2 text-lg font-medium'>Zuri Books</button>
         </a>
        </div>
 
        <div>
         <a href='https://books.zuri.team/'>
-            <button id='book__python'className='bg-[#EAECF0] w-11/12 h-14 rounded-lg border-[#EAECFO] my-2 text-lg font-medium'>Python Books</button>
+            <button id='book__python'className='bg-[#EAECF0] hover:bg-[#D0D5DD] w-11/12 h-14 rounded-lg border-[#EAECFO] my-2 text-lg font-medium'>Python Books</button>
         </a>
        </div>
 
        <div>
         <a href='https://background.zuri.team/'>
-            <button id='pitch'className='bg-[#EAECF0] w-11/12 h-14 rounded-lg border-[#EAECFO] my-2 text-lg font-medium'>Background Check for Coders</button>
+            <button id='pitch'className='bg-[#EAECF0] hover:bg-[#D0D5DD] w-11/12 h-14 rounded-lg border-[#EAECFO] my-2 text-lg font-medium'>Background Check for Coders</button>
         </a>
        </div>
 
        <div>
         <a href='https://books.zuri.team/design-rules'>
-            <button id='book_design'className='bg-[#EAECF0] w-11/12 h-14 rounded-lg border-[#EAECFO] my-2 text-lg font-medium'>Design Books</button>
+            <button id='book_design'className='bg-[#EAECF0] hover:bg-[#D0D5DD] w-11/12 h-14 rounded-lg border-[#EAECFO] my-2 text-lg font-medium'>Design Books</button>
         </a>
        </div>
     </div>
