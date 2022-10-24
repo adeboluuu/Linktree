7dc5684c7547ed273924d967798462acb6af8657
Adeboluwarin <adeboluwarinthomas@gmail.com>
2022-10-24 22:58:38 +0100

    Alligned the profile image and icon horizontally

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
+import icon2 from '../assets/Type=more, State=Pressed.png'
+import icon from '../assets/Type=Share, State=Default.png'
 import image from '../assets/Device=Web, State=Default.png'
 
 const main = () => {
   return (
     <div className='my-20 mx-auto text-center w-11/12'>
-       <img src = {image} alt='profile-image' id='profile__img'className='mx-auto'/>
-       <h1 className='font-bold mb-6 'id='twitter'>adeboluuu</h1>
+        <div className='ml-10'>
+            {/* Profile section */}
+        <div className='profile__section '>
+         <img src = {image} alt='profile-image' id='profile__img'className='mx-auto'/>
+         <span>
+         <img src={icon} alt='Share__icon'className='sm:visible invisible '/>
+         <img src={icon2} alt='Share__icon'className= 'sm:invisible visible'/>            
+         </span>
+        </div>
+        <h1 className='font-bold mb-6 mr-10 'id='twitter'>adeboluuu</h1>
        <p className='hidden'id='slack'>Adeboluwarin Thomas</p>
+        </div>
        <div>
         <a href='https://twitter.com/adeboluuu'>
             <button id='twitter__link' className='bg-[#EAECF0] hover:bg-[#D0D5DD] w-11/12 h-14 rounded-lg border-[#EAECFO] my-2 text-lg font-medium'>Twitter Link</button>
