538b14f659887392efbe581a1ab0bdd705cf0d03
Adeboluwarin <adeboluwarinthomas@gmail.com>
2022-10-24 23:36:49 +0100

    Adjusted the footer section

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,7 +5,7 @@ import I4G from '../assets/I4G.png'
 
 const footer = () => {
   return (
-    <div className='flex space-x-80 mx-20 my-16 '>
+    <div className='md:visible invisible flex md:space-x-80 space-x-10 sm:mx-20 mx-0 sm:my-16 my-0'>
       <img src={Zuri} alt='Zuri Internship'className='h-8'/>
       <img src= {Hng} alt='Hng' className='h-4 mt-3'/>
       <img src ={I4G} alt='Ingressive for good'className='h-8'/>
