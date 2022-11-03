f3c1def20ea3e6d505c54097e6c494e982643725
Adeboluwarin <adeboluwarinthomas@gmail.com>
2022-11-03 17:21:04 +0100

    completed form

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import footer from './footer'
+import Footer from '../components/footer'
 
 function contact() {
   return (
+    <div>
     <div className='my-20 mx-auto text-center sm:w-6/12 w-10/12'>
       <div className='header text-left'>
       <h1 className='text-[#181828] text-4xl font-semibold mb-3'>Contact Me</h1>
@@ -10,16 +11,17 @@ function contact() {
       </div>
 
 
-      <form>
+      <form mailto>
+
       <div className='sm:grid grid-cols-2 gap-8 block text-left my-3'>
         <div className="my-3">
-          <label className="block text-sm font-medium mb-2" for="username"> First name</label>
-          <input className="shadow appearance-none h-11 border font-normal rounded-lg w-full py-2 px-3 text-[#667085] leading-tight focus:outline-none focus:shadow " id="first_name" placeholder="Enter your first name" />
+          <label className="block text-sm font-medium mb-2" for="First name"> First name</label>
+          <input autoComplete='on' className="shadow appearance-none h-11 border font-normal rounded-lg w-full py-2 px-3 text-[#667085] leading-tight focus:outline-none focus:shadow " id="first_name" placeholder="Enter your first name" />
         </div>
         
         <div className="my-3 ">
-          <label class="block  text-sm font-medium  mb-2" for="Lastname">Last name</label>
-          <input className="shadow appearance-none border h-11 font-normal rounded-lg w-full py-2 px-3 text-[#667085] leading-tight focus:outline-none focus:shadow-outline"type="text" id="last_name" placeholder="Enter your last name" />
+          <label class="block text-sm font-medium  mb-2" for="Last name">Last name</label>
+          <input autoComplete='on'className="shadow appearance-none border h-11 font-normal rounded-lg w-full py-2 px-3 text-[#667085] leading-tight focus:outline-none focus:shadow" id="last_name" placeholder="Enter your last name" />
         </div>
         
         </div>
@@ -40,10 +42,14 @@ function contact() {
           <span className='font-normal text-base text-[#475467]'>You agree to providing your data to {name} who may contact you.</span>
         </div>
 
-        <button type='submit 'className='bg-[#1570EF] text-white w-full rounded-lg h-12 mt-2'>Send Message</button>
+        <button id='btn__submit'type='submit 'className=' mb-5 bg-[#1570EF] text-white w-full rounded-lg h-12 mt-2'>Send Message</button>
       </form>
-      <footer/>
+
     </div>
+    <Footer/>
+    </div>
+    
+
   )
 }
 
