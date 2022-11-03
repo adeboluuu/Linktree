1507cb4fd7f7452e2bfdc31b960b055c58ca31d8
Adeboluwarin <adeboluwarinthomas@gmail.com>
2022-11-03 17:27:01 +0100

    bug on hosted site

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-      <Route path ='contact' element={ <Contact/>}/>
+      <Route path ='/contact' element={ <Contact/>}/>
       <Route path ='/' element={ <Home/> }/>
       </Routes>
     </div>
