b9f54ad3699fcc8599038f120c058425aa88bdca
Adeboluwarin <adeboluwarinthomas@gmail.com>
2022-11-03 16:50:15 +0100

    completed form

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -6,22 +6,43 @@ function contact() {
     <div className='my-20 mx-auto text-center sm:w-6/12 w-10/12'>
       <div className='header text-left'>
       <h1 className='text-[#181828] text-4xl font-semibold mb-3'>Contact Me</h1>
-      <p className='font-normal'>Hi there, contact me to ask me about anything you have in mind.</p>
+      <p className='font-normal text-[#475467]'>Hi there, contact me to ask me about anything you have in mind.</p>
       </div>
+
+
       <form>
-      <div className='sm:grid grid-cols-2 gap-8 block text-left'>
-        <div className="my-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" for="username"> First name</label>
-          <input className="shadow appearance-none h-11 border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow " id="fullName" placeholder="Full Name" />
+      <div className='sm:grid grid-cols-2 gap-8 block text-left my-3'>
+        <div className="my-3">
+          <label className="block text-sm font-medium mb-2" for="username"> First name</label>
+          <input className="shadow appearance-none h-11 border font-normal rounded-lg w-full py-2 px-3 text-[#667085] leading-tight focus:outline-none focus:shadow " id="first_name" placeholder="Enter your first name" />
+        </div>
+        
+        <div className="my-3 ">
+          <label class="block  text-sm font-medium  mb-2" for="Lastname">Last name</label>
+          <input className="shadow appearance-none border h-11 font-normal rounded-lg w-full py-2 px-3 text-[#667085] leading-tight focus:outline-none focus:shadow-outline"type="text" id="last_name" placeholder="Enter your last name" />
+        </div>
+        
+        </div>
+
+        <div className="my-3 ">
+          <label class="block  text-left text-sm font-medium  mb-2" for="email">Email</label>
+          <input className="shadow appearance-none border h-11 font-normal rounded-lg w-full py-2 px-3 text-[#667085] leading-tight focus:outline-none focus:shadow-outline"type="email" id="email" placeholder="yourname@email.com" />
+        </div>
+
+        <div className="my-3 ">
+          <label class="block  text-left text-sm font-medium  mb-2" for="message">Message</label>
+          <input className="shadow appearance-none border h-[132px] font-normal rounded-lg w-full py-0  text-start px-3 text-[#667085] leading-tight focus:outline-none focus:shadow-outline"type="text" id="message" placeholder="Send me a message and I'll reply you as soon as possible..." />
         </div>
 
 
-                  <div className="my-4 ">
-                    <label class="block  text-sm font-bold mb-2" for="username">Last name</label>
-                    <input className="shadow appearance-none border h-11 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"type="text" id="username" placeholder="Your username" /></div>
-                </div>
+        <div className='text-left mb-4'>
+          <input type='checkbox'className='mr-2 rounded-md border-[#D0D5DD]'></input>
+          <span className='font-normal text-base text-[#475467]'>You agree to providing your data to {name} who may contact you.</span>
+        </div>
+
+        <button type='submit 'className='bg-[#1570EF] text-white w-full rounded-lg h-12 mt-2'>Send Message</button>
       </form>
-      {footer}
+      <footer/>
     </div>
   )
 }
